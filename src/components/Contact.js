cd1cf1b6374b2934f3127f52e07e53478902674b
vishalpandey2005008-cmyk <vishalpandey2005008@gmail.com>
2025-11-06 22:29:16 +0530

    Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <from>
+      UserName<input type='text' name='UserName' /><br/><br/>
+      Email<input type='text' name='Email' /><br/><br/>
+      Password<input type='text' name='Password' /><br/><br/>
+      <input type='Button' value='Login' />
+      </from>
+    </div>
   )
 }
 
