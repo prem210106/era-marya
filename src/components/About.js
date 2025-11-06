9bd3737915c26de6488da88da805449390a8f5d7
f3-bilp <prem.21106patel@gmail.com>
2025-11-06 22:29:02 +0530

    about

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+function About(){
+  return(
+    <div>
+      <h1>About us </h1>
+      <h2>Innovating the Future Through Technology  </h2>
 
-const About = () => {
-  return (
-    <div>About</div>
-  )
+      <p>
+        We are a leading IT solutions and services company committed to driving digital 
+        transformation for businesses worldwide.With a focus on innovation, quality,
+        and client satisfaction, we deliver technology solutions that help organizations
+        achieve sustainable growth and success in an ever-evolving digital landscape.
+      </p>
+     <h2>Our vision</h2>
+      <p>
+      Our mission is to empower businesses through innovative technology, helping
+      them streamline operations, enhance productivity, and create meaningful digital
+      experiences. We strive to make technology accessible, efficient, and impactful 
+      for every organization we serve.
+     </p>
+    </div>
+  );
 }
-
-export default About
