b2b0c4ba5f398b7bdab5d873b2fecb9e6c4bdda8
sohansolanki24 <sohansolanki2405@gmail.com>
2025-11-07 14:49:54 +0530

    changes in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div style={{textAlign: 'center',marginTop: '450px',display: 'flex',height:'25px',padding:'10px',color:'black',justifyContent: 'center',alignItems: 'center',backgroundColor:'silver',}}>
-            Created by Team 3
+    <div style={{textAlign: 'center',marginTop: '450px',display: 'flex',height:'25px',padding:'10px',color:'black',justifyContent: 'center',alignItems: 'left',backgroundColor:'silver',}}>
+           <p>Do visit our website</p>
+           <p>
+            <a href="https://techstrota.com/">TechStota</a>
+          </p>
     </div>
   )
 }
