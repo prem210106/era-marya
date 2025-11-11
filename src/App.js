9303022f08705598ea7a55d5afaf57d81bbf043f
vishalpandey2005008-cmyk <vishalpandey2005008@gmail.com>
2025-11-11 16:15:05 +0530

    11/11/  v import Service in app.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React from 'react'
-import { BrowserRouter,Route,Routes,} from 'react-router-dom';
-import Home from './components/Home'
-import Contact from './components/Contact'
-import Navbar from './components/Navbar'
-import About from './components/About'
-import './components/Navbar.css'
-import Footer from './components/Footer'
-import Offer from './components/Offer'
- 
+  import React from 'react'
+  import { BrowserRouter,Route,Routes,} from 'react-router-dom';
+  import Home from './components/Home'
+  import Contact from './components/Contact'
+  import Navbar from './components/Navbar'
+  import About from './components/About'
+  import './components/Navbar.css'
+  import Footer from './components/Footer'
+  import Offer from './components/Offer'
+  import Service from './components/Service'
+  import Sea from './components/Sea'
+  import './components/Sea.css'
+  import Air from './components/Air'
+  import './components/Air.css'
+  import Custom from './components/Custom'
+  import './components/Custom.css'
+  import Warehousing from './components/Warehousing'
+  import './components/Warehousing.css'
 
-function App() {
-  return(
-    <BrowserRouter>
-    <div style={{textAlign: 'center', marginTop:'10px'}}>
-    <Navbar /> 
-    <Routes>
-    <Route path="/home"element={<Home/>}/>
-      <Route path="/about"element={<About/>}/>
-      <Route path="/contact"element={<Contact/>}/>
-      <Route path="/offer" element={<Offer />}/>
-    </Routes>
-    </div>
-    <Footer/>
-     </BrowserRouter>
-  )
-}
-export default App;
+  function App() {
+    return(
+      <BrowserRouter>
+      <div style={{textAlign: 'center', marginTop:'10px'}}>
+      <Navbar /> 
+      <Routes>
+      <Route path="/home"element={<Home/>}/>
+        <Route path="/about"element={<About/>}/>
+        <Route path="/contact"element={<Contact/>}/>
+        <Route path="/offer" element={<Offer />}/>
+        <Route path="/Service" element={<Service/>}/>
+        <Route path="/Sea" element={<Sea/>}/>
+        <Route path="/Air" element={<Air/>}/>
+        <Route path="/Custom" element={<Custom/>}/>
+        <Route path="/Warehousing" element={<Warehousing/>}/>
+      </Routes>
+      </div>
+      <Footer/>
+      </BrowserRouter>
+    )
+  }
+  export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ function Navbar() {
       <ul className="nav-list">
         <li><a href="/Home">Home</a></li>
         <li><a href="/About">About</a></li>
-        <li><a href="/offer">Services</a></li>
+        <li><a href="/Service">Services</a></li>
         <li><a href="/Contact">Contact</a></li>
       </ul>
     </nav>
